e98eabe87ca473298f165040249511056808a2c2
Nik Ogura <nikogura@apple.com>
2017-02-01 18:25:44 -0800

    tweaked test command

--- tests/Tests.py
+++ tests/Tests.py
@@ -6,7 +6,7 @@ class AppTest(unittest.TestCase):
     def testInit(self):
         self.assertEqual(1, 1)
         subprocess.check_output(
-            "python -m py_compile ../jungle.py",
+            "/usr/bin/env python -m py_compile ../jungle.py",
             stderr=subprocess.STDOUT,
             shell=True)
 
